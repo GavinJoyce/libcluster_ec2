d798338821ee1699f3a8542538b62ba1250b467a
Gavin Joyce <gavinjoyce@gmail.com>
2022-05-24 11:03:19 +0100

    use ExAws for meta data

--- lib/libcluster_ec2.ex
+++ lib/libcluster_ec2.ex
@@ -1,17 +1,20 @@
 defmodule ClusterEC2 do
-  use Tesla
+  require Logger
 
   @moduledoc File.read!("#{__DIR__}/../README.md")
-
-  plug(Tesla.Middleware.BaseUrl, "http://169.254.169.254/latest/meta-data")
 
   @doc """
     Queries the local EC2 instance metadata API to determine the instance ID of the current instance.
   """
   @spec local_instance_id() :: binary()
   def local_instance_id do
-    case get("/instance-id/") do
-      {:ok, %{status: 200, body: body}} -> body
+    try do
+      Logger.info("ClusterEC2.local_instance_id..")
+      config = ExAws.Config.new(:s3)
+      id = ExAws.InstanceMeta.request(config, "http://169.254.169.254/latest/meta-data/instance-id/")
+      Logger.info("ClusterEC2.local_instance_id: #{id}")
+      id
+    rescue
       _ -> ""
     end
   end
@@ -21,8 +24,14 @@ defmodule ClusterEC2 do
   """
   @spec instance_region() :: binary()
   def instance_region do
-    case get("/placement/availability-zone/") do
-      {:ok, %{status: 200, body: body}} -> String.slice(body, 0..-2)
+    try do
+      Logger.info("ClusterEC2.instance_region..")
+      config = ExAws.Config.new(:s3)
+      region = ExAws.InstanceMeta.request(config, "http://169.254.169.254/latest/meta-data/placement/availability-zone/")
+      |> String.slice(0..-2)
+      Logger.info("ClusterEC2.instance_region: #{region}")
+      region
+    rescue
       _ -> ""
     end
   end
